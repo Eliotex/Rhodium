bf33f8939033ab61d9597a3c92f05e1835a2a4e1
Eliotex <eliotex3@gmail.com>
2025-09-08 16:59:31 +0200

    update

--- src/main/java/net/eliotex/rhodium/mixin/IntegratedServerMixin.java
+++ src/main/java/net/eliotex/rhodium/mixin/IntegratedServerMixin.java
@@ -14,6 +14,7 @@ import org.apache.logging.log4j.Logger;
 public abstract class IntegratedServerMixin {
     @Unique private long lastHeadTime = -1L;
     @Unique private double recentTPS = 20.0;
+    @Unique private int newRD = -1;
     @Shadow private static final Logger LOGGER = LogManager.getLogger();
 
     @Inject(method = "setupWorld()V", at = @At("HEAD"))     //calculate current TPS
@@ -33,18 +34,15 @@ public abstract class IntegratedServerMixin {
     private void redirectSetViewDistance(PlayerManager pm, int originalValue) {
         int current = pm.getViewDistance();
         if(current < 12 && originalValue > 11) {
-            pm.setViewDistance(12);                 //Instantly go to 12rd, bc the spawn chunks are loaded anyway
-            LOGGER.info("Changing view distance to {}, from {}", current, 12);
-            return;
+            newRD = 12;                     //Instantly go to 12rd, bc the spawn chunks are loaded anyway
+        } else if (current > originalValue || originalValue < 13) {
+            newRD = originalValue;          //if rd decrease or player wants to increase to a rd smaller than 13
+        } else if (current < originalValue && recentTPS > 14 && recentTPS < 21) {
+            newRD = current + 1;            //increase by 1, if there is no lag and no ticks get skipped
         }
-        if (current > originalValue || originalValue < 13) {
-            pm.setViewDistance(originalValue);      //if rd decrease or player wants to increase to a rd smaller than 13
-            LOGGER.info("Changing view distance to {}, from {}", current, originalValue);
-            return;
-        }
-        if (current < originalValue && recentTPS > 14 && recentTPS < 21) {
-            pm.setViewDistance(current + 1);        //increase by 1, if there is no lag and no ticks get skipped
-            LOGGER.info("Changing view distance to {}, from {}", current, current + 1);
+        if(newRD!=current) {
+            pm.setViewDistance(newRD);
+            LOGGER.info("Changing view distance to {}, from {}", newRD, current);
         }
     }
     
